094b9ca51459dab068e01467ba589f0da213c0ac
Nicholas Mueller <nicholas.mueller@monash.edu>
2023-09-16 16:11:26 +1000

    stashing

--- src/Tests/newtypes.jl
+++ src/Tests/newtypes.jl
@@ -1,7 +1,7 @@
 struct SupportQuantity{T}
-  array::Vector{T}
+  array::AbstractVector{<:AbstractArray{T}}
 
-  function SupportQuantity(array::Array{T}) where {T}
+  function SupportQuantity(array::AbstractVector{<:AbstractArray{T}}) where T
     new{T}(array)
   end
 
@@ -24,6 +24,10 @@ function Base.setindex!(a::SupportQuantity,v,i...)
   a.array[i...] = v
 end
 
+function Base.first(a::SupportQuantity)
+  SupportQuantity([first(testitem(a))])
+end
+
 function Arrays.CachedArray(a::SupportQuantity)
   ai = testitem(a)
   ci = CachedArray(ai)
@@ -31,28 +35,136 @@ function Arrays.CachedArray(a::SupportQuantity)
   for i in eachindex(a.array)
     array[i] = CachedArray(a.array[i])
   end
-  ArrayBlock(array)
+  SupportQuantity(array)
 end
 
 Arrays.testitem(a::SupportQuantity) = testitem(a.array)
 
-function Arrays.lazy_map(k,a::SupportQuantity)
-  lazy_arrays = map(x->lazy_map(k,x),a.array)
+function Arrays.return_value(f,a::SupportQuantity,x::Union{AbstractArray,SupportQuantity}...)
+  a1 = _getter_at_ind(1,a,x...)
+  return_value(f,a1)
+end
+
+function Arrays.return_cache(f,a::SupportQuantity,x::Union{AbstractArray,SupportQuantity}...)
+  sq1 = _get_1st_sq(a,x...)
+  n = length(sq1)
+  val = return_value(f,a,x...)
+  sval = SupportQuantity(val,n)
+  a1 = _getter_at_ind(1,a,x...)
+  cx = return_cache(f,a1)
+  cx,sval
+end
+
+function Arrays.evaluate!(cache,f,a::SupportQuantity,x::Union{AbstractArray,SupportQuantity}...)
+  cx,sval = cache
+  @inbounds for i = eachindex(sval)
+    ai = _getter_at_ind(i,a,x...)
+    sval.array[i] = evaluate!(cx,f,ai...)
+  end
+  sval
+end
+
+function Arrays.return_value(
+  f::Gridap.Fields.BroadcastingFieldOpMap,
+  a::SupportQuantity,
+  x::Vararg{Union{AbstractArray,SupportQuantity}})
+
+  a1 = _getter_at_ind(1,a,x...)
+  return_value(f,a1...)
+end
+
+function Arrays.return_cache(
+  f::Gridap.Fields.BroadcastingFieldOpMap,
+  a::SupportQuantity,
+  x::Vararg{Union{AbstractArray,SupportQuantity}})
+
+  sq1 = _get_1st_sq(a,x...)
+  n = length(sq1)
+  val = return_value(f,a,x...)
+  sval = SupportQuantity(val,n)
+  a1 = _getter_at_ind(1,a,x...)
+  cx = return_cache(f,a1...)
+  cx,sval
+end
+
+function Arrays.evaluate!(
+  cache,
+  f::Gridap.Fields.BroadcastingFieldOpMap,
+  a::SupportQuantity,
+  x::Vararg{Union{AbstractArray,SupportQuantity}})
+
+  cx,sval = cache
+  @inbounds for i = eachindex(sval)
+    ai = _getter_at_ind(i,a,x...)
+    sval.array[i] = evaluate!(cx,f,ai...)
+  end
+  sval
+end
+
+function Arrays.return_value(f,a::AbstractArray,x::SupportQuantity)
+  a1 = _getter_at_ind(1,a,x)
+  return_value(f,a1)
+end
+
+function Arrays.return_cache(f,a::AbstractArray,x::SupportQuantity)
+  n = length(x)
+  val = return_value(f,a,x)
+  sval = SupportQuantity(val,n)
+  a1 = _getter_at_ind(1,a,x)
+  cx = return_cache(f,a1...)
+  cx,sval
+end
+
+function Arrays.evaluate!(cache,f,a::AbstractArray,x::SupportQuantity)
+  cx,sval = cache
+  @inbounds for i = eachindex(sval)
+    ai = _getter_at_ind(i,a,x)
+    sval.array[i] = evaluate!(cx,f,ai...)
+  end
+  sval
+end
+
+function _getter_at_ind(i::Int,x::Union{AbstractArray,SupportQuantity}...)
+  ret = ()
+  @inbounds for xj in x
+    ret = isa(xj,SupportQuantity) ? (ret...,xj[i]) : (ret...,xj)
+  end
+  return ret
+end
+
+function _get_1st_sq(x::Union{AbstractArray,SupportQuantity}...)
+  for xi in x
+    if isa(xi,SupportQuantity)
+      return xi
+    end
+  end
+end
+
+aa = rand(3)
+bb = rand(3)
+cc = SupportQuantity([aa,bb])
+dd = SupportQuantity([bb,aa])
+xx = aa,bb,cc,dd
+_getter_at_ind(1,xx...)
+
+function Arrays.lazy_map(k,a::SupportQuantity,x::Union{AbstractArray,SupportQuantity}...)
+  lazy_arrays = map(eachindex(a)) do i
+    ai = _getter_at_ind(i,a,x...)
+    lazy_map(k,ai...)
+  end
   SupportQuantity(lazy_arrays)
 end
 
-function Arrays.lazy_map(k,a::SupportQuantity,b::AbstractArray...)
-  lazy_arrays = map(x->lazy_map(k,x,b...),a.array)
+function Arrays.lazy_map(k,a::SupportQuantity,b::Vararg{Union{AbstractArray,SupportQuantity}})
+  lazy_arrays = map(eachindex(a)) do i
+    ai = _getter_at_ind(i,a,b...)
+    lazy_map(k,ai...)
+  end
   SupportQuantity(lazy_arrays)
 end
 
-function Arrays.lazy_map(k,a::AbstractArray,b::SupportQuantity)
-  lazy_arrays = map(x->lazy_map(k,a,x),b.array)
-  SupportQuantity(lazy_arrays)
-end
-
-function Arrays.lazy_map(k,a::SupportQuantity,b::SupportQuantity)
-  lazy_arrays = map((x,y)->lazy_map(k,x,y),a.array,b.array)
+function Arrays.lazy_map(k,a::AbstractArray,x::SupportQuantity)
+  lazy_arrays = map(y->lazy_map(k,a,y),x.array)
   SupportQuantity(lazy_arrays)
 end
 
@@ -80,11 +192,11 @@ function get_pt_fields(ptf::PTFunction)
 end
 
 struct NewGenericPTCellField{DS} <: PTCellField
-  cell_field::AbstractArray
+  cell_field::SupportQuantity
   trian::Triangulation
   domain_style::DS
   function NewGenericPTCellField(
-    cell_field::AbstractArray,
+    cell_field::SupportQuantity,
     trian::Triangulation,
     domain_style::DomainStyle)
 
@@ -103,7 +215,7 @@ end
 
 function CellData.CellField(f::PTFunction,trian::Triangulation,::DomainStyle)
   s = size(get_cell_map(trian))
-  cell_field = lazy_map(x->Fill(x,s),get_pt_fields(f))
+  cell_field = SupportQuantity(lazy_map(x->Fill(x,s),get_pt_fields(f)))
   NewGenericPTCellField(cell_field,trian,PhysicalDomain())
 end
 
@@ -134,8 +246,21 @@ resq = q(x)
 # cache = return_cache(evaluate,fi[1],gx[1])
 # evaluate!(cache,evaluate,fi[1],gx[1])
 
-# q_ok = a(μ[1],t)*∇(dv)
-# resq_ok = q_ok(x)
+q_ok = a(μ[1],t)*∇(dv)
+resq_ok = q_ok(x)
+
+function test_ptarray(a::SupportQuantity,b::AbstractArray)
+  a1 = testitem(a)
+  @assert typeof(a1) == typeof(b)
+  @assert all(a1 .≈ b)
+  return
+end
+
+function test_ptarray(a::AbstractArray,b::SupportQuantity)
+  test_ptarray(b,a)
+end
+
+test_ptarray(resq,resq_ok)
 # # ax_ok = map(i->i(x),q_ok.args[1])
 # _f_ok,_x_ok = CellData._to_common_domain(q_ok.args[1],x)
 # cell_field_ok = get_data(_f_ok)
