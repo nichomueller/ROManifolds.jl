1d14c5ca3f7e57a1f44031368d42f38c4cb247f0
NicholasMueller <nicholas.mueller@monash.edu>
2024-02-27 18:44:50 +1100

    deleted old RB files

--- src/RB/RB.jl
+++ src/RB/RB.jl
@@ -1,10 +1,11 @@
 module RB
-using Mabla.Utils
 using Mabla.FEM
 using LinearAlgebra
+using BlockArrays
 using SparseArrays
+using DrWatson
+using Kronecker
 using Serialization
-using NearestNeighbors
 using Gridap
 using Gridap.Algebra
 using Gridap.FESpaces
@@ -17,98 +18,96 @@ using Gridap.MultiField
 using Gridap.ODEs.ODETools
 using Gridap.ODEs.TransientFETools
 
+import Base: *,\
 import StaticArrays: SVector
 import UnPack: @unpack
-import Gridap.Helpers: @check
-import Gridap.Helpers: @unreachable
+import Gridap.Helpers: @abstractmethod,@check,@notimplemented,@unreachable
 import Gridap.Arrays: evaluate!
-import Gridap.Algebra: allocate_matrix
-import Gridap.Algebra: allocate_vector
-import Gridap.Algebra: solve
+import Gridap.Algebra: allocate_matrix,allocate_vector,solve
+import LinearSolve: UMFPACKFactorization
 import PartitionedArrays: tuple_of_arrays
 
-export RBInfo
-export BlockRBInfo
-export ComputationInfo
-export get_norm_matrix
+export tpod
+export gram_schmidt!
+export orth_complement!
+export orth_projection
+include("BasisConstruction.jl")
+
+export AbstractSnapshots
+export BasicSnapshots
+export TransientSnapshots
+export TransientSnapshotsWithDirichletValues
+export CompressedTransientSnapshots
+export SelectedSnapshotsAtIndices
+export InnerTimeOuterParamTransientSnapshots
+export SelectedInnerTimeOuterParamTransientSnapshots
+export NnzSnapshots
+export GenericNnzSnapshots
+export NnzSnapshotsSwappedColumns
 export Snapshots
-export num_space_dofs
-export num_time_dofs
-export num_params
-export collect_solutions
-export NnzArray
-export NnzVector
-export NnzMatrix
-export get_nonzero_val
-export get_nonzero_idx
-export get_nrows
-export recast
+export slow_index
+export fast_index
+export tensor_getindex
+export tensor_setindex!
+export select_snapshots
+export reverse_snapshots
+export get_realization
 export compress
-export collect_residuals_for_trian
-export collect_jacobians_for_trian
+include("Snapshots.jl")
+
+export RBInfo
+export RBSolver
+export fe_solutions
+export ode_solutions
+include("RBSolver.jl")
+
 export RBSpace
+export reduced_fe_space
 export reduced_basis
 export get_basis_space
+export num_reduced_space_dofs
 export get_basis_time
-export num_rb_space_ndofs
-export num_rb_time_ndofs
-export num_rb_ndofs
-export space_time_projection
-export project_recast
-export RBAffineDecomposition
-export RBVecAffineDecomposition
-export RBMatAffineDecomposition
-export GenericRBAffineDecomposition
-export TrivialRBAffineDecomposition
-export RBIntegrationDomain
-export RBContributionMap
-export RBVecContributionMap
-export RBMatContributionMap
-export get_interpolation_idx
-export project_space
-export project_time
-export project_space_time
-export get_reduced_cells
-export collect_reduced_residuals!
-export collect_reduced_jacobians!
-export rb_coefficient!
-export rb_contribution!
-export zero_rb_contribution
-export RBAlgebraicContribution
-export RBVecAlgebraicContribution
-export RBMatAlgebraicContribution
-export collect_compress_rhs
-export collect_compress_lhs
-export collect_compress_rhs_lhs
-export compress_component
-export collect_rhs_contributions!
-export collect_lhs_contributions!
-export collect_rhs_lhs_contributions!
-export RBResults
-export rb_solver
-export post_process
-export plot_results
-export compute_relative_error
-export nearest_neighbor
-export RBBlock
-export BlockSnapshots
-export BlockRBSpace
-export BlockRBAlgebraicContribution
-export BlockRBVecAlgebraicContribution
-export BlockRBMatAlgebraicContribution
-export BlockRBResults
-export get_blocks
-export fe_offsets
-export rb_offsets
-export add_space_supremizers
-export add_time_supremizers
+export num_reduced_times
+export recast
+include("RBSpace.jl")
 
-include("RBInfo.jl")
-include("Snapshots.jl")
-include("NnzArrays.jl")
-include("RBSpaces.jl")
-include("RBAffineDecomposition.jl")
-include("RBAlgebraicContribution.jl")
-include("RBResults.jl")
-include("RBBlocks.jl")
+export RBOperator
+export reduced_operator
+export get_fe_trial
+export get_fe_test
+export allocate_fe_vector
+export allocate_fe_matrix
+export allocate_fe_matrix_and_vector
+export fe_vector!
+export fe_matrix!
+export fe_matrix_and_vector!
+export fe_matrix_and_vector
+include("RBOperator.jl")
+
+export ReducedIntegrationDomain
+export AffineDecomposition
+export AffineContribution
+export BlockAffineDecomposition
+export mdeim
+export get_mdeim_indices
+export reduce_triangulation
+export compress_basis_space
+export combine_basis_time
+export reduced_matrix_vector_form
+export allocate_mdeim_coeff
+export mdeim_coeff!
+export allocate_mdeim_lincomb
+export mdeim_lincomb!
+include("AffineDecomposition.jl")
+
+export RBNonlinearOperator
+export ThetaMethodNonlinearOperator
+include("RBNonlinearOperator.jl")
+
+export ComputationalStats
+export RBResults
+export rb_results
+export load_solve
+export generate_plots
+include("PostProcess.jl")
 end # module
