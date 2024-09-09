4aff914b9ec8de809b74405fe7b8671e8eeebaab
NicholasMueller <nicholas.mueller@monash.edu>
2024-09-09 10:40:00 +1000

    stashing

--- launch_tests.sh
+++ launch_tests.sh
@@ -1,5 +1,5 @@
-# julia --project tests/RB/RBTransient/gen_sol_test.jl
-# julia --project tests/RB/RBTransient/gen_basis_test.jl
-julia --project tests/RB/RBTransient/gen_online_test.jl
+julia --project tests/RB/RBTransient/stokes_tensortrain.jl
+julia --project tests/RB/RBTransient/get_basis_stokes.jl
+julia --project tests/RB/RBTransient/get_online_stokes.jl
 # julia --project tests/RB/RBTransient/gen_online_test_tt.jl
 # julia --project tests/RB/RBTransient/online_test.jl
