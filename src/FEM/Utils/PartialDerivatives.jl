4939e79294fda29ebbb81817918ee3f7d3079908
NicholasMueller <nicholas.mueller@monash.edu>
2025-02-03 17:59:25 +1100

    changed filename

---
+++ src/FEM/Utils/PartialDerivatives.jl
@@ -0,0 +1,49 @@
+"""
+    struct PartialDerivative{N} <: Function end
+
+Map that implements a partial derivative in `Gridap`
+"""
+struct PartialDerivative{N} <: Function end
+
+PartialDerivative{N}(f) where N = Operation(Component{N}())(∇(f))
+
+"""
+    const ∂₁ = PartialDerivative{1}
+"""
+const ∂₁ = PartialDerivative{1}
+
+"""
+    const ∂₂ = PartialDerivative{2}
+"""
+const ∂₂ = PartialDerivative{2}
+
+"""
+    const ∂₃ = PartialDerivative{3}
+"""
+const ∂₃ = PartialDerivative{3}
+
+const MixedDerivative = Union{
+  PartialDerivative{(1,1)},PartialDerivative{(2,2)},PartialDerivative{(3,3)},
+  PartialDerivative{(1,2)},PartialDerivative{(1,3)},PartialDerivative{(2,1)},
+  PartialDerivative{(2,3)},PartialDerivative{(3,1)},PartialDerivative{(3,2)}
+  }
+
+function Arrays.evaluate!(cache,::Broadcasting{<:PartialDerivative{N}},f) where N
+  Broadcasting(Operation(Component{N}()))(Broadcasting(∇)(f))
+end
+
+function PartialDerivative{N}(f::Function,x::Point,fx) where N
+  Component{N}(gradient(f,x,fx))
+end
+
+"""
+"""
+struct Component{N} <: Function end
+
+(f::Component{N})(x...) where N = evaluate(f,x...)
+(f::Component{N})(a::CellField) where N = Operation(f)(a)
+(f::Component{N})(a::Field...) where N = Operation(f)(a...)
+
+(f::Component{N})(v::MultiValue) where N = Component{N}(v)
+Component{N}(v::MultiValue) where N = @notimplemented
+Component{N}(v::MultiValue{Tuple{D}}) where {N,D} = v[N]
